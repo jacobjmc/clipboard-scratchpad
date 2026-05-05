fbb64e38999dd3b6b30f3b7bbcb8ce6ec3aeab0b
jacobjmc <jacobjmc23@gmail.com>
2026-05-06 07:50:51 +1000

    feat: add persisted clip shelf model

--- Sources/ClipboardScratchpad/StoreState.swift
+++ Sources/ClipboardScratchpad/StoreState.swift
@@ -1,12 +1,35 @@
 import Foundation
 
+struct ClipShelfItem: Codable, Identifiable, Equatable {
+    let id: UUID
+    let content: String
+    let capturedAt: Date
+    let sourceAppName: String?
+    let sourceBundleID: String?
+}
+
 struct StoreState: Codable {
     var noteText: String
     var updatedAt: Date?
+    var clips: [ClipShelfItem]
 
-    init(noteText: String, updatedAt: Date? = nil) {
+    init(noteText: String, updatedAt: Date? = nil, clips: [ClipShelfItem] = []) {
         self.noteText = noteText
         self.updatedAt = updatedAt
+        self.clips = clips
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case noteText
+        case updatedAt
+        case clips
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        noteText = try container.decode(String.self, forKey: .noteText)
+        updatedAt = try container.decodeIfPresent(Date.self, forKey: .updatedAt)
+        clips = try container.decodeIfPresent([ClipShelfItem].self, forKey: .clips) ?? []
     }
 }
 
